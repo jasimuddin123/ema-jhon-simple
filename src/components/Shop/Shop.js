787da97e5e020bdbce83cbfe110fec0b5c35f621
jasimuddin123 <jasimuddindxn@gmail.com>
2020-04-08 22:26:50 -0700

    Total Production Price, Shipping Cost, Tax and total Price

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import fakeData from '../../fakeData';
 import { useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
@@ -10,7 +11,7 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     const handleAddProduct = (product) =>{
-        console.log('Product Added',product);
+       
         const newCart =[...cart, product];
         setCart(newCart);
     }
@@ -26,8 +27,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h3>This is Cart</h3>
-                <h5>Order Summary: {cart.length}</h5>
+               <Cart cart={cart}></Cart>
             </div>
         </div>
 
