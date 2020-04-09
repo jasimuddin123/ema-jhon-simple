6f16f8441ff893333c6e9f5a31c25619f3f3d623
jasimuddin123 <jasimuddindxn@gmail.com>
2020-04-08 19:35:22 -0700

    Display More Information in the product Component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import './Product.css';
 const Product = (props) => {
-    console.log(props.product.name);
+    console.log(props.product);
+    const { img, name, seller, price, stock } = props.product;
     return (
-        <div>
-            <h3>This is Product</h3>  
-            <h4>{props.product.name}</h4>
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by:{seller}</small></p>
+                <p>${price}</p>
+                <br />
+                <p><small>Only {stock} left in stock - Order soon</small></p>
+            </div>
+
+
         </div>
     );
 };
